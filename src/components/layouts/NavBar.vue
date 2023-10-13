cd850b5d6d51b4c673bdee9177cbef96b65f5c93
Olga <olboss85@gmail.com>
2023-10-13 21:46:31 +0600

    auth firebase

---
+++ src/components/layouts/NavBar.vue
@@ -0,0 +1,16 @@
+<template> 
+    <div class="side"> 
+        <p-button v-if="!user" @click="googleRegister" label="Войти" icon="pi pi-user" /> 
+        <p-button v-else @click="googleLogout" label="Войти" icon="pi pi-sign-out" /> 
+    </div> 
+</template> 
+ 
+<script setup> 
+    import PButton from 'primevue/button'; 
+    import { useUser } from '@/composables/useUser' 
+ 
+    const { user, googleRegister, googleLogout } = useUser(); 
+</script> 
+ 
+<style scoped> 
+</style>
